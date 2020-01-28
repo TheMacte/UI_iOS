9e8d82a816673f4bdc3e94214749de5588aa1715
Oleg Dm <oleg@acft.pro>
2020-01-28 20:19:30 +0300

    empty project

---
+++ loginView/loginView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loginView
+//
+//  Created by DAN on 28.01.2020.
+//  Copyright © 2020 DAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
