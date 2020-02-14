4e68881a7fbdbc7bb5856435987ab4d9c1ee7261
Oleg Dmitriev <oleg@acft.pro>
2020-02-14 10:55:24 +0300

    п.2 - changes by interface builder - comlete

--- lesson_04/loginView/around Friends/OneFriendCell.swift
+++ lesson_04/loginView/around Friends/OneFriendCell.swift
@@ -13,17 +13,24 @@ class OneFriendCell: UICollectionViewCell {
     @IBOutlet var userNameOfOneUser: UILabel!
     @IBOutlet weak var backView: UIView!
     
+    @IBInspectable var color:UIColor = .red
+    @IBInspectable var radius:CGFloat = 18
+    @IBInspectable var opacity:Float = 0.5
+
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         //userFaceOfoneUser.layer.masksToBounds = true
         userFaceOfoneUser.layer.cornerRadius = 50 // Скругление
         
         //Shadow
         backView.layer.cornerRadius = 50
         backView.layer.masksToBounds = false
-        backView.layer.shadowColor = UIColor.black.cgColor
-        backView.layer.shadowOpacity = 0.5
-        backView.layer.shadowRadius = 8
+        backView.layer.shadowColor = color.cgColor
+
+        backView.layer.shadowOpacity = opacity
+        backView.layer.shadowRadius = radius
         backView.layer.shadowOffset = CGSize.zero
         
 
