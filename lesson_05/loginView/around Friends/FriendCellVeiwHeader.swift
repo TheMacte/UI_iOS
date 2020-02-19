e8fe95abd057849fc4bd941e6d12452716ce73e2
Oleg Dmitriev <oleg@acft.pro>
2020-02-19 17:38:34 +0300

    п.1 completed in general, but background colore and transparence did't realization

---
+++ lesson_05/loginView/around Friends/FriendCellVeiwHeader.swift
@@ -0,0 +1,14 @@
+//
+//  FriendCellVeiwHeader.swift
+//  loginView
+//
+//  Created by Олег Дмитриев on 19.02.2020.
+//  Copyright © 2020 DAN. All rights reserved.
+//
+
+import UIKit
+
+class FriendCellViewHeader : UITableViewHeaderFooterView {
+    @IBOutlet weak var cellHeaderImageView: UIImageView!
+    @IBOutlet weak var cellHeaderLableView: UILabel!
+}
---
+++ lesson_05/loginView/around Groups/GlobalGroupCell.swift
@@ -0,0 +1,28 @@
+//
+//  GlobalGroupCell.swift
+//  loginView
+//
+//  Created by Олег Дмитриев on 10.02.2020.
+//  Copyright © 2020 DAN. All rights reserved.
+//
+
+import UIKit
+
+class GlobalGroupCell: UITableViewCell {
+    
+    @IBOutlet weak var GlobalSearchCellLable: UILabel!
+    @IBOutlet weak var GlobalSearchGroupPicView: UIImageView!
+    /*
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    */
+
+}
