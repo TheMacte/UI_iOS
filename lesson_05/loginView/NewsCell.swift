928810244d93303a97746a5e4a4ea14e8097354f
Oleg Dmitriev <oleg@acft.pro>
2020-02-22 00:36:54 +0300

    Не понимаю в чём проблема

--- lesson_05/loginView/NewsCell.swift
+++ lesson_05/loginView/NewsCell.swift
@@ -2,12 +2,46 @@
 //  NewsCell.swift
 //  loginView
 //
-//  Created by Олег Дмитриев on 21.02.2020.
+//  Created by DAN on 21.02.2020.
 //  Copyright © 2020 DAN. All rights reserved.
 //
 
 import UIKit
 
-class NewsCell: UICollectionViewCell {
+class NewsCell: UITableViewCell {
     
+    @IBOutlet weak var groupImage: UIImageView!
+    @IBOutlet weak var groupName: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var likeCounter: UILabel!
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var shareButton: UIImageView!
+    @IBOutlet weak var commentsButton: UIImageView!
+    @IBOutlet weak var viewsImage: UIImageView!
+    @IBOutlet weak var viewsCounter: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        groupImage.image = nil
+        groupName.text = ""
+        date.text = ""
+        textField.text = ""
+        likeImage.image = nil
+        likeCounter.text = ""
+        newsImage.image = nil
+        shareButton.image = nil
+        commentsButton.image = nil
+        viewsImage.image = nil
+        viewsCounter.text = ""
+        
+    }
 }
---
+++ lesson_05/loginView/NewsTableViewController.swift
@@ -0,0 +1,51 @@
+//ß
+//  NewsTableViewController.swift
+//  loginView
+//
+//  Created by DAN on 21.02.2020.
+//  Copyright © 2020 DAN. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+        
+        let newsText = ["Александр Сергеевич Пушкин"
+        ]
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+        }
+
+        // MARK: - Table view data source
+
+        override func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+            return newsText.count
+        }
+
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as? NewsCell else {
+                   preconditionFailure("Can't deque NewsCell")
+                
+            }
+            
+            //рабочая константа выбора текстов новостей
+            let newsSingleText = newsText[indexPath.row]
+            
+            
+            //заполняем начальными значениями
+            cell.textField.text = newsSingleText
+            cell.textField.translatesAutoresizingMaskIntoConstraints = true
+            cell.textField.sizeToFit()
+            
+            return cell
+        }
+    }
+
