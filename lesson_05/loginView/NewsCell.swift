d25b6c694bfc0a469009f580593dff2220e61333
Oleg Dmitriev <oleg@acft.pro>
2020-02-21 12:28:38 +0300

    CollectionView

---
+++ lesson_05/loginView/NewsCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewsCell.swift
+//  loginView
+//
+//  Created by Олег Дмитриев on 21.02.2020.
+//  Copyright © 2020 DAN. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UICollectionViewCell {
+    
+}
