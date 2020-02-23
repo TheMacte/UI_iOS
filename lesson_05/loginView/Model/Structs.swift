987ad293e3940cb709c9b3dca79ed1d396c75d82
Oleg Dmitriev <oleg@acft.pro>
2020-02-23 22:55:35 +0300

    I coming... (lesson 5)

--- lesson_05/loginView/Model/Structs.swift
+++ lesson_05/loginView/Model/Structs.swift
@@ -25,3 +25,11 @@ struct Groups: NameOf {
     var avatar: UIImage
     var ruName: String
 }
+
+struct News {
+    var auther: String
+    var autherAvatar: UIImage
+    var newsImage: UIImage
+    var newsText: String
+}
+    
