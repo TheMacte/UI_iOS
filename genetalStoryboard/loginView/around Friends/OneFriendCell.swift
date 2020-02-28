29903b19663dfd94ae9bd4f686814137f821f24f
Oleg Dmitriev <oleg@acft.pro>
2020-02-28 15:25:23 +0300

    it works! Но надо причасать код

--- genetalStoryboard/loginView/around Friends/OneFriendCell.swift
+++ genetalStoryboard/loginView/around Friends/OneFriendCell.swift
@@ -14,6 +14,8 @@ let allPik:[String] = ["Alla", "Boris", "Clara", "Dmitriy"]
 class OneFriendCell: UICollectionViewCell {
     // Скругление и тень
     @IBOutlet var userFaceOfoneUser: UIImageView!
+    @IBOutlet weak var userFaceOfoneUserNew: FaceImageView!
+    
     @IBOutlet var userNameOfOneUser: UILabel!
     @IBOutlet weak var backView: UIView!
     
@@ -47,6 +49,9 @@ class OneFriendCell: UICollectionViewCell {
         super.awakeFromNib()
         //userFaceOfoneUser.layer.masksToBounds = true
         userFaceOfoneUser.layer.cornerRadius = 50 // Скругление
+        userFaceOfoneUserNew.layer.cornerRadius = 50 // Скругление
+        userFaceOfoneUser.image = UIImage(named: allPik[photoCounter])
+        userFaceOfoneUserNew.isHidden = true
         
         //Shadow
         backView.layer.cornerRadius = 50
@@ -63,7 +68,6 @@ class OneFriendCell: UICollectionViewCell {
         let tabGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
         likeView1.isUserInteractionEnabled = true
         likeView1.addGestureRecognizer(tabGesture)
-        
         
         
         let swipeRight = UISwipeGestureRecognizer(target: self, action:  #selector(respondToSwipeGesture(_:)))
@@ -92,14 +96,37 @@ class OneFriendCell: UICollectionViewCell {
     }
     
     private func rideSwipeByPhoto(){
+        userFaceOfoneUserNew.image = UIImage(named: allPik[self.photoCounter])
         if(photoCounter < allPik.count - 1){
             photoCounter += 1
         } else {
             photoCounter = 0
         }
-        
         //ВОТ МОМЕНТ АНИМАЦИИ
-        userFaceOfoneUser.image  = UIImage(named: allPik[photoCounter])
+        userFaceOfoneUserNew.frame.origin.x += 106
+        userFaceOfoneUserNew.isHidden = false
+        userFaceOfoneUser.transform = CGAffineTransform(scaleX: 0.5,y: 0.5)
+        userFaceOfoneUser.alpha = 0.01
+        UIView.animateKeyframes(withDuration: 1,
+                                delay: 0,
+                                options: [],
+                                animations: {
+                                    UIView.addKeyframe(withRelativeStartTime: 0,
+                                                       relativeDuration: 1,
+                                                       animations: {
+                                                        self.userFaceOfoneUserNew.transform = CGAffineTransform(scaleX: 0.01,y: 0.01)
+                                                        self.userFaceOfoneUserNew.alpha = 0.5
+                                                        self.userFaceOfoneUser.image = UIImage(named: allPik[self.photoCounter])
+                                                        self.userFaceOfoneUser.transform = CGAffineTransform(scaleX: 1,y: 1)
+                                                        self.userFaceOfoneUserNew.frame.origin.x += 106
+                                                        self.userFaceOfoneUser.alpha = 1
+                                    })
+        },completion: {(true) -> Void in
+            self.userFaceOfoneUserNew.transform = CGAffineTransform(scaleX: 1,y: 1)
+            self.userFaceOfoneUserNew.isHidden = true
+            self.userFaceOfoneUserNew.frame.origin.x -= 212
+            self.userFaceOfoneUserNew.alpha = 1
+        })
     }
     
     private func leftSwipeByPhot(){
@@ -108,32 +135,30 @@ class OneFriendCell: UICollectionViewCell {
         } else {
             photoCounter = allPik.count - 1
         }
-        
-        UIView.animateKeyframes(withDuration: 2,
+        userFaceOfoneUserNew.image = UIImage(named: allPik[self.photoCounter])
+        userFaceOfoneUserNew.transform = CGAffineTransform(scaleX: 0.01,y: 0.01)
+        userFaceOfoneUserNew.alpha = 0.01
+        userFaceOfoneUserNew.isHidden = false
+        UIView.animateKeyframes(withDuration: 1,
                                 delay: 0,
                                 options: [],
                                 animations: {
                                     UIView.addKeyframe(withRelativeStartTime: 0,
-                                                       relativeDuration: 0.25,
+                                                       relativeDuration: 1,
                                                        animations: {
+                                                        self.userFaceOfoneUserNew.transform = CGAffineTransform(scaleX: 1,y: 1)
                                                         self.userFaceOfoneUser.transform = CGAffineTransform(scaleX: 0.5,y: 0.5)
-                                                        //self.userFaceOfoneUser.image  = UIImage(named: allPik[self.photoCounter])
-                                                        
+                                                        self.userFaceOfoneUserNew.frame.origin.x += 106
+                                                        self.userFaceOfoneUserNew.alpha = 1
+                                                        self.userFaceOfoneUser.alpha = 0.01
                                     })
-                                    UIView.addKeyframe(withRelativeStartTime: 0.25,
-                                                       relativeDuration: 0.25,
-                                                       animations: {
-                                                        self.userFaceOfoneUser.transform = CGAffineTransform(scaleX: 1,y: 1)
-                                                        
-                                    })
-                                    //self.userFaceOfoneUser.image  = UIImage(named: allPik[self.photoCounter])
-                                    //ВОТ МОМЕНТ АНИМАЦИИ
-                                    //userFaceOfoneUser.image  = UIImage(named: allPik[photoCounter])
-        },completion: nil
-            //self.userFaceOfoneUser.image  = UIImage(named: allPik[self.photoCounter])
-        )
-        //self.userFaceOfoneUser.image  = UIImage(named: allPik[self.photoCounter])
-
+        },completion: {(true) -> Void in
+            self.userFaceOfoneUser.transform = CGAffineTransform(scaleX: 1,y: 1)
+            self.userFaceOfoneUser.image  = UIImage(named: allPik[self.photoCounter])
+            self.userFaceOfoneUserNew.isHidden = true
+            self.userFaceOfoneUserNew.frame.origin.x -= 106
+            self.userFaceOfoneUser.alpha = 1
+        })
     }
     //Обрабатываем нажатия
     @objc func tapped(_ sender: UITapGestureRecognizer) {
