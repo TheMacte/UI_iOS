349dc456ed36778500a0797c97cfd9d627fba930
Oleg Dmitriev <oleg@acft.pro>
2020-03-03 12:51:28 +0300

    вроде всё реализовано, но не работает жест пальцем в сторону. Нет анимации

--- genetalStoryboard/loginView/around Friends/CustomNavigationController.swift
+++ genetalStoryboard/loginView/around Friends/CustomNavigationController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class CustomNavigationController: UINavigationController, UINavigationControllerDelegate {
+    private let interactiveTransition = CustomInteractiveTransition()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
     }
     
+    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactiveTransition.hasStarted ? interactiveTransition : nil
+    }
+    
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
         if operation == .push{
+            self.interactiveTransition.viewController = toVC
             return CustomPushAnimater()
         }
         else if operation == .pop{
+            if navigationController.viewControllers.first != toVC {
+                 self.interactiveTransition.viewController = toVC
+            }
             return CustomPopAnimater()
         }
         
--- genetalStoryboard/loginView/around Friends/CustomPopPushTransitioning.swift
+++ genetalStoryboard/loginView/around Friends/CustomPopPushTransitioning.swift
@@ -39,6 +39,7 @@ class CustomPushAnimater: NSObject, UIViewControllerAnimatedTransitioning {
         }) { (finished) in
             if finished && !transitionContext.transitionWasCancelled{
                 destination.view.transform = .identity
+                source.view.transform = .identity
             }
             transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
         }
@@ -54,13 +55,8 @@ class CustomPopAnimater: NSObject, UIViewControllerAnimatedTransitioning {
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         guard let source = transitionContext.viewController(forKey: .from),
             let destination = transitionContext.viewController(forKey: .to) else { return }
-        
-        transitionContext.containerView.addSubview(destination.view)
-        destination.view.frame = source.view.frame
-        destination.view.transform = .init(rotationAngle: .pi / 2)
-        destination.view.frame.origin.x -= destination.view.frame.width
-        destination.view.frame.origin.y -= (destination.view.frame.height - destination.view.frame.width) / 2
-        
+
+        transitionContext.containerView.insertSubview(destination.view, belowSubview: source.view)
         UIView.animateKeyframes(withDuration: self.transitionDuration(using: transitionContext),
                                 delay: 0,
                                 options: .calculationModePaced,
@@ -69,13 +65,12 @@ class CustomPopAnimater: NSObject, UIViewControllerAnimatedTransitioning {
                                     UIView.addKeyframe(withRelativeStartTime: 0,
                                                        relativeDuration: 0.75,
                                                        animations: {
-                                                        destination.view.transform = destination.view.transform.rotated(by: -.pi / 2)
-                                                        destination.view.frame.origin.x = 0
-                                                        destination.view.frame.origin.y = 0
+                                                            source.view.transform = source.view.transform.rotated(by: -.pi / 2)
+                                                            source.view.frame.origin.x += source.view.frame.width
                                     })
         }) { (finished) in
             if finished && !transitionContext.transitionWasCancelled{
-                destination.view.transform = .identity
+                source.view.transform = .identity
             }
             transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
         }
