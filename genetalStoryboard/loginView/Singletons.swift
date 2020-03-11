635b0e6a19586b7e4950abf71e5d866f2add4294
Oleg Dmitriev <oleg@acft.pro>
2020-03-11 20:42:18 +0300

    homeWork_01 by iOS network.

---
+++ genetalStoryboard/loginView/Singletons.swift
@@ -0,0 +1,23 @@
+//
+//  Singletons.swift
+//  loginView
+//
+//  Created by Олег Дмитриев on 11.03.2020.
+//  Copyright © 2020 DAN. All rights reserved.
+//
+
+import UIKit
+
+class Session {
+    private init(){}
+    
+    static let shared: Session = .init();
+    
+    var token: String = .init();
+    var userId: Int = .init();
+
+    func changeSwssion(token: String,  userId: Int){
+        self.token = token
+        self.userId = userId
+    }
+}
